b0bedd983ba1ff3efac9873644634c69ada2a3b6
Jacob Holtom <jacob@holtom.me>
2015-12-26 23:58:23 -0500

    magic

--- caffe/classify.py
+++ caffe/classify.py
@@ -4,10 +4,8 @@ import tempfile
 import os
 caffe_root = '/opt/caffe/'
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 pd.set_option('display.mpl_style', 'default')
-%matplotlib inline
 
 caffe_root = '/opt/caffe/'
 
@@ -15,9 +13,6 @@ import sys
 sys.path.insert(0, caffe_root + 'python')
 
 import caffe
-plt.rcParams['figure.figsize'] = (4, 4)
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
 try:
     labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
 except:
@@ -25,8 +20,6 @@ except:
     labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
 # Set Caffe to CPU mode because who even knows how to set up a GPU (not me)
 caffe.set_mode_cpu()
-
-# Load our model! trained by the GOOGLES! <3
 def load_model():
     BATCH_SIZE = 1
     net = caffe.Net('/opt/caffe/models/bvlc_googlenet/deploy.prototxt',
@@ -47,21 +40,6 @@ transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
 transformer.set_transpose('data', (2,0,1))
 transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
 transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
-
-
-#MAGIC
-cat_data = transformer.preprocess('data', caffe.io.load_image(caffe_root + 'examples/images/cat.jpg'))
-#prints the info (image type (3 = RGB), pixels x pixels)
-print cat_data.shape
-
-
-net.blobs['data'].data[...] = cat_data
-out = net.forward()
-pred_class = out['prob'][0].argmax()
-print("Predicted class is #{}.".format(pred_class))
-
-print labels[pred_class]
-
 
 #FUNCTIONS
 def get_label_name(num):
@@ -90,3 +68,17 @@ def predict(data, n_preds=6, display_output=True):
             print format_string.format(
                 cls=pred_formatted, label=get_label_name(pred), certainty=percent)
     return prob
+
+#MAGIC
+imagepath = '/images/test.jpg'
+cat_data = transformer.preprocess('data', caffe.io.load_image(imagepath))
+#prints the info (image type (3 = RGB), pixels x pixels)
+print cat_data.shape
+
+
+net.blobs['data'].data[...] = cat_data
+out = net.forward()
+pred_class = out['prob'][0].argmax()
+print("Predicted class is #{}.".format(pred_class))
+
+print labels[pred_class]

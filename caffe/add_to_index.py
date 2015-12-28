95cbb4a468fd803076a54d975ce8d0786c08baf5
Jacob Holtom <jacob@holtom.me>
2015-12-27 22:51:16 -0500

    place in new place

--- caffe/add_to_index.py
+++ caffe/add_to_index.py
@@ -1,11 +1,6 @@
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
-def produce_data(imagepath):
-    image_data = transformer.preprocess('data', caffe.io.load_image(imagepath))
-    x = predict(image_data)
-    return x
-
 def add_document(data):
     obj_type = data['type']
     predictions = data['predict']
--- caffe/classify.py
+++ caffe/classify.py
@@ -52,3 +52,8 @@ def start_network():
     transformer.set_transpose('data', (2,0,1))
     transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
     transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+
+def produce_data(imagepath):
+    image_data = transformer.preprocess('data', caffe.io.load_image(imagepath))
+    x = predict(image_data)
+    return x

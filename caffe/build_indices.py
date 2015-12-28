d8098c73cde3163347b7d6a01da51b65ed798b6e
Jacob Holtom <jacob@holtom.me>
2015-12-27 20:31:00 -0500

    holy crap

--- caffe/build_indices.py
+++ caffe/build_indices.py
@@ -1,78 +1,8 @@
-import shutil
-import requests
-import tempfile
-import os
-caffe_root = '/opt/caffe/'
-import numpy as np
-caffe_root = '/opt/caffe/'
-import sys
-sys.path.insert(0, caffe_root + 'python')
-import caffe
-imagenet_labels_filename = caffe_root + '/data/ilsvrc12/synset_words.txt'
-labels = np.loadtxt(imagenet_labels_filename, str, delimiter='\t')
-# Set Caffe to CPU mode because who even knows how to set up a GPU (not me)
-caffe.set_mode_cpu()
+from datetime import datetime
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-def load_model():
-    BATCH_SIZE = 1
-    net = caffe.Net('/opt/caffe/models/bvlc_googlenet/deploy.prototxt',
-                    '/models/bvlc_googlenet.caffemodel',
-                    caffe.TEST)
-    # change batch size to 1 for faster processing
-    # this just means that we're only processing one image at a time instead of like 50
-    shape = list(net.blobs['data'].data.shape)
-    shape[0] = BATCH_SIZE
-    net.blobs['data'].reshape(*shape)
-    net.blobs['prob'].reshape(BATCH_SIZE, )
-    net.reshape() 
-    return net
-
-net = load_model()
-# Caffe comes with a handy transformer pipeline so that
-# we can make our images into the format it needs! Yay!
-transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-transformer.set_transpose('data', (2,0,1))
-transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
-transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
-
-#FUNCTIONS
-def get_label_name(num):
-    options = labels[num].split(',')
-    # remove the tag
-    options[0] = ' '.join(options[0].split(' ')[1:])
-    return ','.join(options[:2])
-    
-def predict(data, n_preds=6, display_output=True):
-    net.blobs['data'].data[...] = data
-    if display_output:
-        display(data)
-    prob = net.forward()['prob']
-    probs = prob[0]
-    prediction = probs.argmax()
-    top_k = probs.argsort()[::-1]
-    for pred in top_k[:n_preds]:
-        percent = round(probs[pred] * 100, 2)
-        # display it compactly if we're displaying more than the top prediction
-        pred_formatted = "%03d" % pred
-        if n_preds == 1:
-            format_string = "label: {cls} ({label})\ncertainty: {certainty}%"
-        else:
-            format_string = "label: {cls} ({label}), certainty: {certainty}%"
-        if display_output:
-            print format_string.format(
-                cls=pred_formatted, label=get_label_name(pred), certainty=percent)
-    return prob
-
-#MAGIC
-#imagepath = '/images/test.jpg'
-imagepath = raw_input('Full Path to image to classify > ')
-cat_data = transformer.preprocess('data', caffe.io.load_image(imagepath))
-#prints the info (image type (3 = RGB), pixels x pixels)
-print cat_data.shape
-
-net.blobs['data'].data[...] = cat_data
-out = net.forward()
-pred_class = out['prob'][0].argmax()
-print("Predicted class is #{}.".format(pred_class))
-
-print labels[pred_class]
+f = open("/opt/caffe/data/ilsvrc12/synset_words.txt")
+for line in f:
+    type = line.split()[0]
+    es.index(index='classifcations', doc_type=type)
--- caffe/es.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from elasticsearch import Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-
-f = open("/opt/caffe/data/ilsvrc12/synset_words.txt")
-for line in f:
-    type = line.split()[0]
-    es.index(index='classifcations', doc_type=type)

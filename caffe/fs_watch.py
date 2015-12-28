7ad47e3285d0c6a17232617ac0867dd4b57dddca
Jacob Holtom <jacob@holtom.me>
2015-12-28 12:13:10 -0500

    add change watcher

--- caffe/fs_watch.py
+++ caffe/fs_watch.py
@@ -1,4 +1,10 @@
 import os
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
 def all_files(directory):
     for path, dirs, files in os.walk(directory):
         for f in files:
@@ -23,3 +29,18 @@ def is_image(f):
 def find_images():
     image_files = [f for f in all_files(your_directory) if is_image(f)]
     return image_files
+
+def watch_for_changes(path): # This method needs to be daemonized
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()

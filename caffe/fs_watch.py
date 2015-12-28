8c7c9f3cb90f0a9c0611b8083ec0eb53558d2b9d
Jacob Holtom <jacob@holtom.me>
2015-12-28 12:11:20 -0500

    add in some source

---
+++ caffe/fs_watch.py
@@ -0,0 +1,25 @@
+import os
+def all_files(directory):
+    for path, dirs, files in os.walk(directory):
+        for f in files:
+            yield os.path.join(path, f)
+
+def is_image(f):
+    if f.endswith('.JPG'):
+	return True
+    if f.endswith('.jpg'):
+	return True
+    if f.endswith('.JPEG'):
+	return True
+    if f.endswith('.jpeg'):
+	return True
+    if f.endswith('.png'):
+	return True
+    if f.endswith('.PNG'):
+	return True
+    else:
+	return False
+
+def find_images():
+    image_files = [f for f in all_files(your_directory) if is_image(f)]
+    return image_files
